11d70886edb5d353c9d6a4771ed124fa2cfa765f
Tharunda De Silva <tharundadesilva@gmail.com>
2025-08-04 20:04:15 +0530

    navbars mobile responsiveness fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,14 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className='flex items-center justify-between relative  w-screen h-30  bg-white/6 backdrop-blur-2xs z-51 text-white top-0 left-0  '>
+    <div className='flex items-center justify-between fixed  w-screen h-30  bg-white/6 backdrop-blur-2xs z-51 text-white top-0 left-0  '>
 
         <div className='flex items-center'>
 
             <img src={profile} alt=""  className='w-20 ms-10 rounded-full border-3'/>
-            <h1 className='jersey ms-10 text-6xl '>Tharunda De Silva</h1>
+            <h1 className='jersey ms-10 text-2xl lg:text-7xl '>Tharunda De Silva</h1>
+            <i class="ri-menu-3-line lg:hidden ms-10 text-2xl"></i>
+
             
         </div>
 
