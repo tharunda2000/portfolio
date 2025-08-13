bcde3d19422e1dccaeeb0d5c58f8281e228d2583
Tharunda De Silva <tharundadesilva@gmail.com>
2025-08-13 13:57:13 +0530

    about page alignments fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,12 +14,12 @@ const About = () => {
         <div className='flex flex-col lg:flex-row w-screen h-screen absolute'>
 
             <div className='flex lg:flex-col justify-center '>
-                <img src={profile} alt="" srcset="" className='w-80 lg:w-100 lg:ms-60 mt-45 lg:mt-60 border-5 border-white' />
+                <img src={profile} alt="" srcset="" className='w-80 lg:w-100 lg:ms-60 mt-45 lg:mt-30 border-5 border-white' />
             </div>
 
             <div className='flex lg:  flex-col lg:ms-25 justify-center items-center  '>
-                <h1 className='relative text-7xl jersey text-gray-400 text-center lg:mt-55  mt-10 '>ABOUT ME</h1>
-                <p className='w-100 lg:w-250 text-2xl lg:text-5xl text-center text-gray-100 mt-15 jersey  '>I'm an undergraduate at the University of Sri Jayewardenepura, currently pursuing a Full Stack Web Development course at ICET Institute. Passionate about building end-to-end web solutions, I specialize in both frontend and backend technologies. My tech stack includes Java, JavaScript, Spring Boot, Node.js, React, MySQL, and MongoDB. I enjoy transforming complex problems into user-friendly applications that are scalable, secure, and efficient.</p>
+                <h1 className='relative text-7xl jersey text-gray-400 text-center lg:mt-25  mt-10 '>ABOUT ME</h1>
+                <p className='w-100 px-8 lg:px-0 lg:w-250 text-2xl lg:text-5xl text-center text-gray-100 mt-5 lg:mt-15 jersey mb-10 lg:mb-0 '>I'm an undergraduate at the University of Sri Jayewardenepura, currently pursuing a Full Stack Web Development course at ICET Institute. Passionate about building end-to-end web solutions, I specialize in both frontend and backend technologies. My tech stack includes Java, JavaScript, Spring Boot, Node.js, React, MySQL, and MongoDB. I enjoy transforming complex problems into user-friendly applications that are scalable, secure, and efficient.</p>
             </div>
 
         </div>
